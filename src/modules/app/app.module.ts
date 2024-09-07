1e6f9347383461131d6f6a5bf689de7672247635
aminvv <aminviper1378@gmail.com>
2024-09-07 18:29:13 +0330

    folder to put

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { ConfigModule } from "@nestjs/config";
 import { join } from 'path';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TypeOrmConfig } from './config/typeormConfig';
+import { TypeOrmConfig } from '../../config/typeormConfig';
 @Module({
   imports: [ConfigModule.forRoot({envFilePath:join(process.cwd(),'.env')}),
     TypeOrmModule.forRoot(TypeOrmConfig())
   ],
-  controllers: [AppController],
-  providers: [AppService],
+
 })
 export class AppModule {}
