1cbb84f910143e37f0a26b0a86db9115619b04fe
aminvv <aminviper1378@gmail.com>
2024-09-18 17:10:14 +0330

    check otp code ------------>>>>(chapter:6 section:14)

--- src/common/enums/message.enum.ts
+++ src/common/enums/message.enum.ts
@@ -11,6 +11,7 @@ NotFoundAccount="حساب کاربری یافت نشد",
 AlreadyExistAccount="حساب کاربری با این مشخصات وجود دارد",
 ExpiredCode="کد تایید منقضی شده مجددا تلاش کنید",
 TryAgain="دوباره تلاش کنید",
+loginAgain="مجددا وارد حساب کاربری خود شوید"
 }
 
 export enum NotFoundMessage{
@@ -22,5 +23,6 @@ export enum ValidationMessage{
 }
 
 export enum publicMessage{
-    SendOtp="کد یکبار مصرف با موفقیت ارسال شد"
+    SendOtp="کد یکبار مصرف با موفقیت ارسال شد",
+    loggedIn="با موفقیت وارد حساب خود شدید"
 }
