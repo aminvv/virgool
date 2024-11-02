9541b01ce4903373a363e8ce820f943042ba38e2
aminvv <aminviper1378@gmail.com>
2024-11-02 17:37:47 +0330

    Migration related settings and configurations--------->>>>>>>>(chapter:7 section:2)

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -1,4 +1,5 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { config } from 'dotenv';
+import { join } from 'path';
 import { BlogCategoryEntity } from "src/modules/blog/entities/blog.category.entity";
 import { BlogEntity } from "src/modules/blog/entities/blog.entity";
 import { BookmarksEntity } from "src/modules/blog/entities/bookmark.entity";
@@ -10,19 +11,20 @@ import { followEntity } from "src/modules/user/entities/follow.entity";
 import { OtpEntity } from "src/modules/user/entities/otp.entity";
 import { ProfileEntity } from "src/modules/user/entities/profile.entity";
 import { UserEntity } from "src/modules/user/entities/user.entity";
+import { DataSource } from "typeorm";
 
-export function TypeOrmConfig(): TypeOrmModuleOptions {
-    const { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USERNAME, } = process.env
-    return {
-        host: DB_HOST,
-        port: DB_PORT,
-        database: DB_NAME,
-        username: DB_USERNAME,
-        password: DB_PASSWORD,
-        autoLoadEntities: false,
-        entities: [UserEntity,OtpEntity,ProfileEntity,CategoryEntity,BlogLikesEntity,BlogEntity,CommentsEntity,BookmarksEntity,BlogCategoryEntity,ImageEntity,followEntity],
-        synchronize: false,
-        type: 'postgres',
-    }
-}
-   
+config({ path: join(process.cwd(), ".env") });
+const {DB_HO5ST,DB_PASSWORD,DB_PORT,DB_USERNAME,DB_NAME}=process.env
+let dataSource= new DataSource({
+    type:"postgres",
+    host:DB_HO5ST,
+    port:DB_PORT,
+    username:DB_USERNAME,
+    password:DB_PASSWORD,
+    database:DB_NAME,
+    synchronize:false,
+    entities :[UserEntity,OtpEntity,ProfileEntity,CategoryEntity,BlogLikesEntity,BlogEntity,CommentsEntity,BookmarksEntity,BlogCategoryEntity,ImageEntity,followEntity],
+    migrations:['dist/migrations/*{.ts,.js}'],
+    migrationsTableName:"virgool_migration_db"
+})
+export default dataSource
