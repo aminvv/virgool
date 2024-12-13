2634bd37502994799dd1f87568ac004ccefc9e01
aminvv <aminviper1378@gmail.com>
2024-12-13 12:26:13 +0330

    Editing a column's properties in Migration ----------->>>>>>>(chapter:7 section:6)

--- src/migration/1732271479668-$npm_config_name.ts
+++ src/migration/1732271479668-$npm_config_name.ts
@@ -1,37 +1,45 @@
 import { Roles } from "@common/common/enums/role.enum";
 import { UserStatus } from "@common/common/enums/status.enum";
 import { EntityName } from "src/common/enums/entity.enum";
-import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { MigrationInterface, QueryRunner, Table, TableColumn } from "typeorm";
 
 export class  $npmConfigName1732271479668 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(
-            new Table({
-                name:"User",
-                columns:[
-                    {name:"id",isPrimary:true, type:"serial",isNullable:true},
-                    {name:"username", type:"character varying(50)",isNullable:true,isUnique:true},
-                    {name:"phone", type:"character varying(12)",isNullable:true,isUnique:true},
-                    {name:"email", type:"character varying(100)",isNullable:true,isUnique:true},
-                    {name:"role", type:"enum",enum:[Roles.Admin,Roles.User]},
-                    {name:"status", type:"enum",enum:[UserStatus.Block,UserStatus.Report],isNullable:true},
-                    {name:"new_email", type:"varchar(100)",isNullable:true},
-                    {name:"new_phone", type:"varchar(100)",isNullable:true},
-                    {name:"verify_phone", type:"boolean",isNullable:true,default:false},
-                    {name:"verify_email", type:"boolean",isNullable:true,default:false},
-                    {name:"password", type:"varchar(20)",isNullable:true},
-                    {name:"created_at", type:"timestamp",default:"now()"},
-                ]
-            })
-            ,true  //not user folder write true if notExist
-        )
+        // await queryRunner.createTable(
+        //     new Table({
+        //         name:EntityName.User,
+        //         columns:[
+        //             {name:"id",isPrimary:true, type:"serial",isNullable:true},
+        //             {name:"username", type:"character varying(50)",isNullable:true,isUnique:true},
+        //             {name:"phone", type:"character varying(12)",isNullable:true,isUnique:true},
+        //             {name:"email", type:"character varying(100)",isNullable:true,isUnique:true},
+        //             {name:"role", type:"enum",enum:[Roles.Admin,Roles.User]},
+        //             {name:"status", type:"enum",enum:[UserStatus.Block,UserStatus.Report],isNullable:true},
+        //             {name:"new_email", type:"varchar(100)",isNullable:true},
+        //             {name:"new_phone", type:"varchar(100)",isNullable:true},
+        //             {name:"verify_phone", type:"boolean",isNullable:true,default:false},
+        //             {name:"verify_email", type:"boolean",isNullable:true,default:false},
+        //             {name:"password", type:"varchar(20)",isNullable:true},
+        //             {name:"created_at", type:"timestamp",default:"now()"},
+        //         ]
+        //     })
+            // ,true  //not user folder write true if notExist
+        // )
             //@ts-ignore
-            await queryRunner.addColumn("User",{name:"balance",type:"numeric",isNullable:true ,default:0})
+            // await queryRunner.addColumn(EntityName.User,{name:"balance",type:"numeric",isNullable:true ,default:0})
+        
+                                 // To manually delete a database
+        // const balance=await queryRunner.hasColumn(EntityName.User,"balance")
+        // if(!balance) await queryRunner.addColumn(EntityName.User,{name:"balance",type:"numeric",isNullable:true ,default:0})
 
-                                     // To manually delete a database
-            // const balance=await queryRunner.hasColumn(EntityName.User,"balance")
-            // if(!balance) await queryRunner.addColumn(EntityName.User,{name:"balance",type:"numeric",isNullable:true ,default:0})
+            const username=await queryRunner.hasColumn(EntityName.User,"username")
+            if(username){await queryRunner.changeColumn(EntityName.User,"username", new TableColumn({
+                name:"username",
+                type:"varchar(40)",
+                isNullable:true,
+                isUnique:true,
+            }))}
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
@@ -40,8 +48,7 @@ export class  $npmConfigName1732271479668 implements MigrationInterface {
         // or userTable ===>>> (true)
         await queryRunner.dropTable(EntityName.User,true)
 
-        // if not exist   .dropTable  write=
-                                            //await queryRunner.dropColumn(EntityName.User,"balance")
+        // if not exist   .dropTable  write=  await queryRunner.dropColumn(EntityName.User,"balance")
 
     }
 
